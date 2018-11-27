a5842e29d441fb9789a9d9534eb5680c10e7f2ac
Sean Escriva <sean.escriva@gmail.com>
2018-11-27 16:48:28 -0500

    Revert ill advised pluralization .gsub method

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -6,9 +6,8 @@ module SensuCookbook
     end
 
     # Pluralize object directory name
-    # gsub because English is weird
     def object_dir
-      ::File.join(new_resource.config_home, type_from_name).gsub('y', 'ie') + 's'
+      ::File.join(new_resource.config_home, type_from_name) + 's'
     end
 
     def object_file
