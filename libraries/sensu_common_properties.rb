f3e5c0e8b3afd948e0f5b8151d3d323f6c08328c
Derek Groh <derekgroh@gmail.com>
2019-07-23 13:57:25 -0500

    revert d4c5dc3bc84270f9ac1051f050b98bf6f861c2e7

--- libraries/sensu_common_properties.rb
+++ libraries/sensu_common_properties.rb
@@ -3,8 +3,7 @@ module SensuCookbook
     # lazy load the common properties
     def self.included(base)
       base.class_eval do
-        property :config_home, String, default: '/etc/sensu' if node['platform'] != 'windows'
-        property :config_home, String, default: 'c:/ProgramData/Sensu/config' if node['platform'] == 'windows'
+        property :config_home, String, default: '/etc/sensu'
       end
     end
   end
