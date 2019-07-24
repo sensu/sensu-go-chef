7a63eaa099a6f44eceb850ac7fcca2432e9f3609
Derek Groh <derekgroh@gmail.com>
2019-07-24 13:01:58 -0500

    restore centos-7 version to 7.3.1611

--- spec/support/platforms.rb
+++ spec/support/platforms.rb
@@ -42,7 +42,7 @@ RSpec.shared_context 'centos-7' do
     ChefSpec::SoloRunner.new(
       os: 'linux',
       platform: 'centos',
-      version: '7.6.1804',
+      version: '7.3.1611',
       file_cache_path: '/var/chef/cache'
     ).converge(described_recipe)
   end
