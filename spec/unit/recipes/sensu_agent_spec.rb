afade5559cd51da9ab51813d54c3a3421855e571
Derek Groh <derekgroh@gmail.com>
2019-08-22 11:32:24 -0500

    fauxhai change for case sensitive - remove agent

--- spec/unit/recipes/sensu_agent_spec.rb
+++ spec/unit/recipes/sensu_agent_spec.rb
@@ -190,7 +190,7 @@ RSpec.describe 'sensu_test::remove_agent' do
     'centos' => '7.6.1804',
   }
   win_platforms = {
-    'windows' => %w(2012r2 2016 2019),
+    'windows' => %w(2012R2 2016 2019),
   }
 
   nix_platforms.each do |platform, versions|
