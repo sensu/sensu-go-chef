6cccd0f1de182e97353e58b2996b840ae923937a
Cameron Johnston <cameron@sensu.io>
2019-11-21 12:51:01 -0700

    modify type_from_name helper to support multi-word resource names, e.g. postgres_config

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -2,7 +2,8 @@ module SensuCookbook
   module Helpers
     # Extract object type from resource type
     def type_from_name
-      new_resource.declared_type.to_s.split('_').last
+      name_elements = new_resource.declared_type.to_s.split('_')
+      name_elements.drop(1).join('_')
     end
 
     # Pluralize object directory name
