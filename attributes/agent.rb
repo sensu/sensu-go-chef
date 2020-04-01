ac10978540e74c86d6f15fd9d26e8c37de103ab8
Conor Tinch <ctinch@cloud.ca>
2020-04-01 10:27:33 -0400

    Added flag to choose to restart agent on configuration change.

--- attributes/agent.rb
+++ attributes/agent.rb
@@ -3,3 +3,4 @@ default['sensu-go']['windows_msi_source'] = 'https://s3-us-west-2.amazonaws.com/
 default['sensu-go']['windows_msi_checksum'] = '776577450E7063FD78D937EA3C0AF6B93F20017499E692DD3FA6ED8BA0DC8835'
 default['sensu-go']['msi_version'] = '5.11.1'
 default['sensu-go']['conf_restart_immediate'] = false
+default['sensu-go']['agent_restart_on_conf_change'] = true
--- resources/agent.rb
+++ resources/agent.rb
@@ -57,8 +57,8 @@ action :install do
     # render template at /etc/sensu/agent.yml for linux
     file ::File.join(new_resource.config_home, 'agent.yml') do
       content(JSON.parse(new_resource.config.to_json).to_yaml.to_s)
-      notifies :restart, 'service[sensu-agent]' unless node['sensu-go']['conf_restart_immediate']
-      notifies :restart, 'service[sensu-agent]', :immediately if node['sensu-go']['conf_restart_immediate']
+      notifies :restart, 'service[sensu-agent]' unless node['sensu-go']['conf_restart_immediate'] || node['sensu-go']['agent_restart_on_conf_change'] == false
+      notifies :restart, 'service[sensu-agent]', :immediately if node['sensu-go']['conf_restart_immediate'] && node['sensu-go']['agent_restart_on_conf_change']
     end
 
     # Enable and start the sensu-agent service
@@ -86,8 +86,8 @@ action :install do
     # render template at c:\Programdata\Sensu\config\agent.yml for windows
     file ::File.join('c:/ProgramData/Sensu/config/', 'agent.yml') do
       content(JSON.parse(new_resource.config.to_json).to_yaml.to_s)
-      notifies :restart, 'service[SensuAgent]', :delayed unless node['sensu-go']['conf_restart_immediate']
-      notifies :restart, 'service[SensuAgent]', :immediately if node['sensu-go']['conf_restart_immediate']
+      notifies :restart, 'service[SensuAgent]' unless node['sensu-go']['conf_restart_immediate'] || node['sensu-go']['agent_restart_on_conf_change'] == false
+      notifies :restart, 'service[SensuAgent]', :immediately if node['sensu-go']['conf_restart_immediate'] && node['sensu-go']['agent_restart_on_conf_change']
     end
 
     # Installs SensuAgent Service
