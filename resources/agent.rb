cc3e40a7a6b8c523d1dafc8447ef52103e44974a
kovukono <kovukono@hotmail.com>
2020-05-13 01:35:55 -0400

    Restart agent on config change to load new changes. (#87)
    Co-authored-by: Conor Tinch <ctinch@cloud.ca>

--- resources/agent.rb
+++ resources/agent.rb
@@ -57,6 +57,7 @@ action :install do
     # render template at /etc/sensu/agent.yml for linux
     file ::File.join(new_resource.config_home, 'agent.yml') do
       content(JSON.parse(new_resource.config.to_json).to_yaml.to_s)
+      notifies :restart, 'service[sensu-agent]', :delayed
     end
 
     # Enable and start the sensu-agent service
@@ -84,6 +85,7 @@ action :install do
     # render template at c:\Programdata\Sensu\config\agent.yml for windows
     file ::File.join('c:/ProgramData/Sensu/config/', 'agent.yml') do
       content(JSON.parse(new_resource.config.to_json).to_yaml.to_s)
+      notifies :restart, 'service[SensuAgent]', :delayed
     end
 
     # Installs SensuAgent Service
